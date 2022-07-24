06690656cd9a133a522504a18413f3fc7d6b7584
Marlene Guzman <41800372+mardb@users.noreply.github.com>
2022-07-24 16:29:34 -0700

    basic routes working properly

--- routes.js
+++ routes.js
@@ -74,29 +74,39 @@ router.post(
 //COURSES
 
 //  returns all courses including the User associated with each course and a 200 HTTP status code.
-router.get(
-  '/courses',
-  asyncHandler(async (req, res) => {
-    res.json(users);
+router.get( '/courses', asyncHandler(async (req, res) => {
+    const courses = await Course.findAll()
+    //TODO: include the User associated with each course
+    res.status(200).json({courses})
   })
 );
+
 // return the corresponding course including the User associated with that course and a 200 HTTP status code.
-// router.get('/courses/:id', asyncHandler(async(req, res) => {
-//   res.json(users);
-// }));
+router.get('/courses/:id', asyncHandler(async(req, res) => {
+  const course = await Course.findOne()
+   //TODO: include the User associated with each course
+  res.status(200).json({course})
+}));
 
 // create a new course, set the Location header to the URI for the newly created course, and return a 201 HTTP status code and no content.
-// router.post('/courses/', asyncHandler(async(req, res) => {
-//   res.json(users);
-// }));
+router.post('/courses/', asyncHandler(async(req, res) => {
+const course = await Course.create(req.body);
+  res.status(201).set('Location', `courses/${course.id}`).end();
+}));
 
 //updates the corresponding course and return a 204 HTTP status code and no content.
-// router.put('/courses/:id',asyncHandler(async (req, res) => {
-//   res.json(users);
-// }));
+router.put('/courses/:id',asyncHandler(async (req, res) => {
+  const course = await Course.findByPk(req.params.id);
+  await course.update(req.body);
+  res.status(204).end()
+}));
+
 //deletes the corresponding course - returns a 204 HTTP status code and no content.
-// router.delete('/courses/:id', asyncHandler(async(req, res) => {
-//   res.json(users);
-// }));
+router.delete('/courses/:id', asyncHandler(async(req, res) => {
+  const course = await Course.findByPk(req.params.id);
+  await course.destroy()
+  res.status(204).end()
+
+}));
 
 module.exports = router;
